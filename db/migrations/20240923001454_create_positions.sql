11bde4acab92a537c894f17cb6a805056b7d9c13
Pepe <pepe.valdivia.caballero@gmail.com>
2024-09-22 20:53:21 -0500

    sqls de creacion de tablas e inserts de play_styles

--- db/migrations/20240923001454_create_positions.sql
+++ db/migrations/20240923001454_create_positions.sql
@@ -1,5 +1,11 @@
 -- migrate:up
 
+CREATE TABLE positions (
+  id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  acronym	VARCHAR(5),
+  name	VARCHAR(25)
+);
 
 -- migrate:down
 
+DROP TABLE IF EXISTS positions;
--- db/migrations/20240923001531_create_leagues.sql
+++ db/migrations/20240923001531_create_leagues.sql
@@ -1,5 +1,12 @@
 -- migrate:up
 
+CREATE TABLE leagues (
+  id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name	VARCHAR(30),
+  nation_id	INTEGER NOT NULL,
+  FOREIGN KEY (nation_id) REFERENCES nations (id)
+);
 
 -- migrate:down
 
+DROP TABLE IF EXISTS leagues;
